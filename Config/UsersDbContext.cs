89236546223a05db8e6f28a59a75b8082e2c94b2
jandrocode <jandrocode@gmail.com>
2024-06-28 13:17:01 +0200

    Initial

---
+++ Config/UsersDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CrudUsuarios.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudUsuarios.Config
+{
+    public class UsersDbContext : DbContext
+    {
+        public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options) { }
+
+
+        public DbSet<User> Usuarios{ get; set; }
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrudUsuarios.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]   
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
+}
